de73e0255d5a8329773b05963c7cf42ef2caaed2
LauraStorm <lbs2rm@hotmail.com>
2022-10-14 14:07:43 +0200

    update

--- src/MyClass2.java
+++ src/MyClass2.java
@@ -1,6 +1,6 @@
 public class MyClass2 {
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("Hej :-)");
         System.out.println("med dig!");
     }
 }
